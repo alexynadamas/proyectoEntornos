93d762f6ec36070c4fe195d46c04b91c8648474a
Alex <alexclementefornas95@gmail.com>
2020-04-06 23:26:29 +0200

    Se configura el MenuButton

--- Estadistica/src/estadistica/FXMLDocumentController.java
+++ Estadistica/src/estadistica/FXMLDocumentController.java
@@ -21,7 +21,7 @@ import javafx.scene.control.RadioButton;
  * @author Victoria (1Daw)
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     private Label label;
     @FXML
     private MenuButton menu;
@@ -31,25 +31,63 @@ public class FXMLDocumentController implements Initializable {
     private RadioButton hombre;
     @FXML
     private RadioButton mujer;
-    
+
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
-    }    
+
+    }
 
     @FXML
     private void enviarInformacion(ActionEvent event) {
-       
     }
 
     @FXML
     private void seleccionSexo(ActionEvent event) {
     }
 
-    
+    @FXML
+    private void andalucia(ActionEvent event) {
+        menu.setText("Andalucía");
+    }
+
+    @FXML
+    private void aragon(ActionEvent event) {
+        menu.setText("Aragón");
+    }
+
+    @FXML
+    private void castillaYLeon(ActionEvent event) {
+        menu.setText("Catilla y León");
+    }
+
+    @FXML
+    private void cataluña(ActionEvent event) {
+        menu.setText("Cataluña");
+    }
+
+    @FXML
+    private void comunidadValenciana(ActionEvent event) {
+        menu.setText("Comunidad Valenciana");
+    }
+
+    @FXML
+    private void galicia(ActionEvent event) {
+        menu.setText("Galicia");
+    }
+
+    @FXML
+    private void comunidadDeMadrid(ActionEvent event) {
+        menu.setText("Comunidad de Madrid");
+    }
+
+    @FXML
+    private void paisVasco(ActionEvent event) {
+        menu.setText("País Vasco");
+    }
+
 }
